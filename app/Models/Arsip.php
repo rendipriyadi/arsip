bd9afb198dd3e4a5d105c9805b94498e98c483ef
andiyudi <49513145+andiyudi@users.noreply.github.com>
2023-02-03 11:09:43 +0700

    add model arsip

---
+++ app/Models/Arsip.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Arsip extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'arsipid',
+        'no_arsip',
+        'klasifikasi',
+        'bentuk_arsip',
+        'uraian',
+        'kode_unit',
+        'jenis_arsip',
+        'ruas',
+        'unit_p',
+        'm_k',
+        'tahun',
+        'jumlah',
+        's_d',
+        'no_box',
+        'perkembangan',
+        'lokasi',
+        'retensi',
+        'gudang',
+        'keterangan',
+        'kelompok',
+        'proyek',
+        'indeks',
+    ];
+}
