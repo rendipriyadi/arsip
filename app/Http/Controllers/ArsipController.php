f9db60b82a1f744a57b0210abb81a6f43f292b08
andiyudi <49513145+andiyudi@users.noreply.github.com>
2023-02-03 11:44:39 +0700

    add route arsip test

--- app/Http/Controllers/ArsipController.php
+++ app/Http/Controllers/ArsipController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Arsip;
 
 class ArsipController extends Controller
 {
-    //
+    public function index()
+    {
+        //
+    }
 }
