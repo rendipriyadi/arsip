84bb938c9e7776e12680581f4bc3532753c2c4f2
andiyudi <49513145+andiyudi@users.noreply.github.com>
2023-02-07 15:32:44 +0700

    make dummy card dashboard view

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,9 +1,9 @@
 <!-- partial:partials/_footer.html -->
 <footer class="footer">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-      <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Divisi Teknologi Informasi <a href="{{ asset('') }}assets/https://www.bootstrapdash.com/" target="_blank">PT. Citra Marga Nusaphala Persada Tbk.</a> Seksi Sistem Informasi.</span>
+      <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Divisi Teknologi Informasi <a href="http://id.citramarga.com" target="_blank">PT. Citra Marga Nusaphala Persada Tbk.</a> </span>
       <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2023 - {{ now()->year }}
-        . All rights reserved.</span>
+        . Seksi Sistem Informasi.</span>
     </div>
   </footer>
   <!-- partial -->
